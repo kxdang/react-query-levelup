428b3234ec54b0793b67a450609e74eb8af652bc
unknown <ikien.dang@gmail.com>
2022-12-02 11:50:44 -0500

    feat(queryclient): initialize the queryClient and render issues list and handled loading state

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,23 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
+  const { data, isLoading } = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
+
   return (
     <div>
       <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {data.map((issue) => (
+            <IssueItem {...issue} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
