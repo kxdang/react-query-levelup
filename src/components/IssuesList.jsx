cab9c605ac46dcc8e2b08178a521b97d9998f50c
unknown <ikien.dang@gmail.com>
2023-03-06 19:20:38 -0500

    finished episode 12

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
-export default function IssuesList({ labels }) {
-  const { data, isLoading } = useQuery(["issues", { labels }], () => {
+export default function IssuesList({ labels, status }) {
+  const { data, isLoading } = useQuery(["issues", { labels, status }], () => {
+    const statusString = status ? `&status=${status}` : "";
     const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
-    return fetch(`/api/issues?${labelsString}`).then((res) => res.json());
+    return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
+      res.json()
+    );
   });
 
   return (
